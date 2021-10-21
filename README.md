d584f9e6d5d5dac28c374b3cc210501028fc698b
maxicuyo94 <maxibc94@gmail.com>
2021-10-21 12:58:16 -0300

    api init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Boilerplate
+
+### Instrucciones para ejecutar el proyecto
+
+- El proyecto tiene dos carpetas `api` y `client`. En estas carpetas estará el código del back-end y el front-end respectivamente.
+
+- En `api` crear un archivo llamado: `.env` que tenga la siguiente forma:
+
+```
+  DB_USER=usuariodepostgres
+  DB_PASSWORD=passwordDePostgres
+  DB_HOST=localhost
+```
+
+- Reemplazar `usuariodepostgres` y `passwordDePostgres` con tus propias credenciales para conectarte a postgres.
+
+- Adicionalmente será necesario que creen desde psql una base de datos llamada `boilerplate`
+
+- El contenido de `client` fue creado usando: Create React App.
+
+- En ambas carpetas ejecutar
+  > npm i
+  >
+  > npm start
---
+++ api/src/app.js
@@ -0,0 +1,35 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
+
+const server = express();
+
+server.name = "API";
+
+server.use(express.urlencoded()); //Parse URL-encoded bodies
+server.use(express.json());
+server.use(cookieParser());
+server.use(morgan("dev"));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+server.use("/", routes);
+
+// Error catching endware.
+server.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
